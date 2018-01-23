a3d7759d14f2ec19ab5238e94596bc34da9cc0be
Santhosh Siripragada <Santhosh.Siripragada@Lnttechservices.com>
2018-01-23 10:17:35 +0530

    committing after latest codeblocks modification

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 //#include "first_program.h"
 #include <cstdint>
+#include <bitset>
 
 using namespace std;
 
@@ -99,9 +100,102 @@ void Task1(void)
         cout<<"Result is:"<<fVariable1/fVariable2<<endl;
 }
 
+const float Gravity_Constant=9.8;
+void Distance_Fallen(float Tower_height,int time)
+{
+    float Distance_fallen = Tower_height - Gravity_Constant*time*time/2;
+    if (Distance_fallen <=0)
+        Distance_fallen = 0;
+    cout<<"Ball at "<<time<<"Sec is at height\t"<<Distance_fallen<<"\tin Meters"<<endl;
+
+}
+
+void Ball_Dropped_Example(void)
+{
+    float Tower_height;
+    cout<<"Please provide the tower height in Meters\t";
+    cin>> Tower_height;
+    Distance_Fallen(Tower_height, 1);
+    Distance_Fallen(Tower_height, 2);
+    Distance_Fallen(Tower_height, 3);
+    Distance_Fallen(Tower_height, 4);
+    Distance_Fallen(Tower_height, 5);
+}
+
+void Print_Binary_number(void)
+{
+    int16_t nTemp,nInput;
+    int Divisor=128;
+    cout<<"Please provide a number between 0 to 255\t";
+    cin>>nInput;
+    cout<<"Binary representation of number is: ";
+    while(Divisor>0)
+    {
+        nTemp=nInput/Divisor;
+        //cout<<nTemp<<"\n";
+        if(nTemp==1)
+            cout<<"1";
+        else cout<<"0";
+        nInput%=Divisor;
+        Divisor /=2;
+        nTemp=0;
+        //cout<<"Divisor is:"<<Divisor<<endl;
+    }
+}
+
+//options corresponds to bit indices
+const int option1 = 0;
+const int option2 = 1;
+const int option3 = 2;
+const int option4 = 3;
+const int option5 = 4;
+const int option6 = 5;
+const int option7 = 6;
+const int option8 = 7;
+void Bit_Set_Example(void)
+{
+    std::bitset<20> bits(0x0); // we need 8 bits, start with bit pattern 0000 0010
+    bits.set(option5); // set bit 4 to 1 (now we have 0001 0010)
+    bits.flip(option6); // flip bit 5 (now we have 0011 0010)
+    bits.reset(option6); // set bit 5 back to 0 (now we have 0001 0010)
+
+    std::cout << "Bit 4 has value: " << bits.test(option5) << '\n';
+    std::cout << "Bit 5 has value: " << bits.test(option6) << '\n';
+    std::cout << "All the bits: " << bits << '\n';
+}
+
+void Variable_Scope_Example(void)
+{
+    int nVariable1, nVariable2;
+    cout<<"Please enter first Variable:\t";
+    cin>>nVariable1;
+    cout<<"Please enter second Variable:\t";
+    cin>>nVariable2;
+    if(nVariable2<nVariable1)
+    {
+        int temp=nVariable2;
+        nVariable2=nVariable1;
+        nVariable1=temp;
+    }
+    cout<<"First Variable is:\t"<<nVariable1<<endl;
+    cout<<"Second Variable is:\t"<<nVariable2<<endl;
+}
+int g_Variable =20;
+void GlobalVariableVsLocalVatiableScope(void)
+{
+    int g_Variable =10;
+    g_Variable++;
+    cout<<"The variable g_Variable after increment from 10 is:\t"<<g_Variable<<endl;
+    ::g_Variable--;
+    cout<<"The variable g_Variable after decrement from 20 is:\t"<<::g_Variable<<endl;
+}
 int main()
 {
     //code comes here
+    //Print_Binary_number();
+    //Bit_Set_Example();
+    //Variable_Scope_Example();
+    GlobalVariableVsLocalVatiableScope();
 
     return 0;
 }
